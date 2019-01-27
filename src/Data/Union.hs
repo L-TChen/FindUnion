7ea1159c99f381a7dc26da83118ca061e83a409a
Liang-Ting Chen <liang.ting.chen.tw@gmail.com>
2019-01-27 20:53:24 +0000

    Add code document

--- src/Data/Union.hs
+++ src/Data/Union.hs
@@ -1,12 +1,25 @@
-{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE TupleSections, Trustworthy #-}
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Data.Union.ST
+-- License     :  (see the file LICENSE)
+--
+-- Maintainer  :  Liang-Ting Chen
+-- Stability   :  experimental
+-- Portability :  non-portable
+
+-----------------------------------------------------------------------------
 
 module Data.Union
   ( Union (..)
+  -- * Creation of disjoint unions
   , buildDS
   , buildDSWith
-  , toList
+  -- * Conversion between lists of lists and disjoint unions
   , fromList
   , fromListWith
+  , toList
+  -- * Find-related operations of Find-Union 
   , connected
   , find
   , size
@@ -24,29 +37,37 @@ instance (Ix i, Show i, Show m) => Show (Union i m) where
   show u = showString "fromList " 
            $ shows (bounds $ getUnion u) $ showString " " $ show $ toList u
 
-toList :: (Ix i) => Union i m -> [m]
-toList = map trd3 . components 
-
+-- | Construct a collection of disjoint sets from a list of lists. Two lists are in the same set if there is any overlap. 
 fromList :: (Ix i) => (i, i) -> [[i]] -> Union i [i]
 fromList bnd = fromListWith bnd (:[])
 
+-- | Similar to the above but takes an assignment of representatives to elements of a semigroup.
 fromListWith :: (Ix i, Semigroup m) => (i, i) -> (i -> m) -> [[i]] -> Union i m
 fromListWith bnd f = buildDSWith bnd f . concatMap pairs
   where 
     pairs []     = [] -- shall not include this clause 
     pairs (x:xs) = map (x,) xs 
 
+-- | Return the list of disjoint lists
+toList :: (Ix i) => Union i m -> [m]
+toList = map trd3 . components 
+
+-- | Check if a pair of representatives belong to the same set. 
 connected :: (Ix i) => i -> i -> Union i m -> Bool 
 connected i j u = find i u == find j u
 
+-- | Return the canonical representative for the set containing the argument.
 find :: (Ix i, Eq i) => i -> Union i m -> i
 find i (Union u) = fst3 $ u!i 
 
+-- | Return the size of set which contains the argument.
 size :: (Ix i) => i -> Union i m -> Int
 size i u'@(Union u) = snd3 (u ! find i u')
 
+-- | Return the set containing the argument and apply semigroup operation to every element.
 component :: (Ix i) => i -> Union i m -> m
 component i u'@(Union u) = trd3 (u ! find i u')
 
+-- | Return the collection of disjoint sets, their sizes and canonical representatives. 
 components :: (Ix i) => Union i m -> [(i, Int, m)]
 components (Union u) = filter (\(_, s, _) -> s /= 0) $ elems u
--- src/Data/Union/ST.hs
+++ src/Data/Union/ST.hs
@@ -1,17 +1,32 @@
-{-# LANGUAGE BangPatterns #-} 
+{-# LANGUAGE BangPatterns, Trustworthy #-} 
+
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Data.Union.ST
+-- License     :  (see the file LICENSE)
+--
+-- Maintainer  :  Liang-Ting Chen
+-- Stability   :  experimental
+-- Portability :  non-portable
+
+-----------------------------------------------------------------------------
 
 module Data.Union.ST
   ( STUnion
   , Union (..)
+  -- * Creation
   , create
   , createWith
+  , buildDS
+  , buildDSWith
+  -- * Find-related operations 
+  , find
   , connected
   , component
   , components
-  , find
+  -- * Union-related operations
+  , union
   , unions
-  , buildDS
-  , buildDSWith
   ) where
 
 import Data.Array
@@ -23,23 +38,27 @@ import Control.Monad.ST
 
 import Data.Union.Internal
 
+-- | The type of collections of disjoint unions to be used within the 'Control.Monad.ST'.
 type STUnion s i m = STArray s i (i, Int, m) 
-type Bound i = (i, i)
 
-newtype Union i m = Union { getUnion :: Array i (i, Int, m) }
-
+-- | The type of collections of disjoint unions using persistent array.
+newtype Union i m = Union { getUnion :: Array i (i, Int, m) -- ^ Retrieve the underlying array representation
+                    }
 parent :: (Ix i) => i -> STUnion s i m -> ST s i
 parent i u = fst3 <$> readArray u i
 
+-- | Check if two representatives are in the same set.
 connected :: (Ix i, Semigroup m) => i -> i -> STUnion s i m -> ST s Bool
 connected i j u = (==) <$> find i u <*> find j u
 
 size :: (Ix i) => (i, Int, m) -> Int
 size (_, n, _) = n
 
+-- | Return the set of elements containing the given argument (after applying the monoid operation)
 component :: (Ix i, Semigroup m) => i -> STUnion s i m -> ST s m
 component i u = find i u >>= fmap trd3 . readArray u
 
+-- | Return the collection of disjoint sets of type 'm', its size, and its representative. 
 components :: (Ix i, Semigroup m) => STUnion s i m -> ST s [(i, Int, m)] 
 components u = do 
   bnd <- getBounds u
@@ -47,6 +66,7 @@ components u = do
   xss <- getAssocs u
   return [ (par, s, xs) | (i, (par, s, xs)) <- xss, s /= 0 ]
 
+-- | Find the representative of the set to which the arguments belongs.
 find :: (Ix i, Eq i, Semigroup m) => i -> STUnion s i m -> ST s i
 find i u = do
   !j <- parent i u
@@ -54,14 +74,17 @@ find i u = do
     !bnd <- getBounds u
     compress bnd i j u 
 
-create :: (Ix i) => Bound i -> ST s (STUnion s i [i])
+-- | Create an *n*-element collection of singleton disjoint sets.
+create :: (Ix i) => (i, i) -> ST s (STUnion s i [i])
 create bnd = createWith bnd (:[])
 
-createWith :: (Ix i, Semigroup m) => Bound i -> (i -> m) -> ST s (STUnion s i m)
+-- | Similar to the above but assigning to every representative an element of some semigroup.
+createWith :: (Ix i, Semigroup m) => (i, i) -> (i -> m) -> ST s (STUnion s i m)
 createWith bnd f = newListArray bnd $ zip3 xs [1,1..] (map f xs)
   where 
     xs = range bnd
 
+-- | Union of two sets which contain the arguments respectively.
 union :: (Ix i, Semigroup m) => i -> i -> STUnion s i m -> ST s ()
 union i j u = do
   !pi <- find i u 
@@ -72,10 +95,11 @@ union i j u = do
     y <- unsafeRead u (index bnd pj)
     if size x < size y then link bnd x y u else link bnd y x u
 
+-- | Similar to the above, but takes a set of pairs instead. 
 unions :: (Ix i, Semigroup m) => [(i, i)] -> STUnion s i m -> ST s ()
 unions reps u = forM_ reps (\(i, j) -> union i j u)
 
-compress :: (Ix i, Semigroup m) => Bound i -> i -> i -> STUnion s i m -> ST s i
+compress :: (Ix i, Semigroup m) => (i, i) -> i -> i -> STUnion s i m -> ST s i
 compress bnd i j u = do
     (_, _, xs) <- unsafeRead u (index bnd i) 
     !pi        <- unsafeFind bnd j u 
@@ -83,28 +107,30 @@ compress bnd i j u = do
     return pi
 
 link :: (Ix i, Semigroup m) 
-    => Bound i -> (i, Int, m) -> (i, Int, m) -> STUnion s i m -> ST s ()
+    => (i, i) -> (i, Int, m) -> (i, Int, m) -> STUnion s i m -> ST s ()
 link bnd (i, !n, xs) (j, !m, ys) u = do
   unsafeWrite u (index bnd i) (j, 0,   xs)
   unsafeWrite u (index bnd j) (j, n+m, xs <> ys)
 
-rebuild :: (Ix i, Semigroup m) => Bound i -> STUnion s i m -> ST s () 
+rebuild :: (Ix i, Semigroup m) => (i, i) -> STUnion s i m -> ST s () 
 rebuild bnd u = forM_ (range bnd) $ \i -> unsafeFind bnd i u
 
-buildDS :: (Ix i) => Bound i -> [(i, i)] -> Union i [i]
+-- | Create a collection of disjoint sets using 'union'. 
+buildDS :: (Ix i) => (i, i) -> [(i, i)] -> Union i [i]
 buildDS bnd = buildDSWith bnd (:[]) 
 
-buildDSWith :: (Ix i, Semigroup m) => Bound i -> (i -> m) -> [(i, i)] -> Union i m
+-- | Similar to the above, but takes an additional function assigning to to every representative an element of some semigroup. 
+buildDSWith :: (Ix i, Semigroup m) => (i, i) -> (i -> m) -> [(i, i)] -> Union i m
 buildDSWith bnd f reps = Union $ runSTArray $ do 
   u <- createWith bnd f 
   unions reps u 
   rebuild bnd u 
   return u
 
-unsafeParent :: (Ix i, Semigroup m) => Bound i -> i -> STUnion s i m -> ST s i
+unsafeParent :: (Ix i, Semigroup m) => (i, i) -> i -> STUnion s i m -> ST s i
 unsafeParent bnd i u = fst3 <$> unsafeRead u (index bnd i)
 
-unsafeFind :: (Ix i, Eq i, Semigroup m) => Bound i -> i -> STUnion s i m -> ST s i
+unsafeFind :: (Ix i, Eq i, Semigroup m) => (i, i) -> i -> STUnion s i m -> ST s i
 unsafeFind bnd i u = do
   !j <- unsafeParent bnd i u
   if i == j then return j else compress bnd i j u 
